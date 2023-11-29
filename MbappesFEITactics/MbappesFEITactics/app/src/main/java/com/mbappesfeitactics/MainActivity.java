a6e593ece2d280a7f928b2680dbdfadbe25e47bf
Javier Durán Torres <118077906+TRENCH13@users.noreply.github.com>
2023-11-29 16:09:59 -0600

    Update MainActivity.java
    Inicio de Sesión corregido

--- MbappesFEITactics/MbappesFEITactics/app/src/main/java/com/mbappesfeitactics/MainActivity.java
+++ MbappesFEITactics/MbappesFEITactics/app/src/main/java/com/mbappesfeitactics/MainActivity.java
@@ -1,7 +1,6 @@
 package com.mbappesfeitactics;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,94 +23,53 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
-    private JugadorService jugadorService;
-
-    Jugador jugadorObtenido = new Jugador();
-
-    boolean banderaConexion = false;
-
-    boolean banderaUsuario = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater()); //Para accesar a los datos del layout
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        //Componentes de la vista
-        //setContentView(R.layout.activity_main);
-        EditText gamertagUsuario = findViewById(R.id.gamertag);
-        EditText passwordUsuario = findViewById(R.id.password);
-        Button btnInciarSesion = findViewById(R.id.btnIniciarSesion);
-        Button btnIniciarInvitado = findViewById(R.id.btnIniciarInvitado);
-        Button btnCrearCuenta = findViewById(R.id.btnCrearCuenta);
-        TextView txtAlerta = findViewById(R.id.lbErrorInicio);
 
-        //Configuración de retrofit
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://mk2m8b3x-3000.usw3.devtunnels.ms")
-                .addConverterFactory(GsonConverterFactory.create()).build();
-
-        //Instancia del JugadorDAO
-
-        jugadorService = retrofit.create(JugadorService.class);
-
-
-        btnInciarSesion.setOnClickListener(v->{
-            jugadorObtenido=null;
-            banderaConexion=true;
-            banderaUsuario=false;
-            String gamertag = gamertagUsuario.getText().toString();
-            String password = passwordUsuario.getText().toString();
-            Log.d("Test",gamertag);
-            Log.d("Test",password);
+        binding.btnIniciarSesion.setOnClickListener(v->{
+            String gamertag = binding.gamertag.getText().toString();
+            String password = binding.password.getText().toString();
             if(!gamertag.isEmpty() || !password.isEmpty()) {
-
-                Log.d("PRUEBAGG","Evaluacion");
-
                 loginUser(gamertag, password);
-
-                if(!banderaConexion){
-                    openMenuPrincipal(gamertag);
-                }else{
-                    Toast.makeText(this, "Error en la conexion", Toast.LENGTH_SHORT).show();
-                }
-
-
             }else{
-                Toast.makeText(this,"Error al iniciar sesión", Toast.LENGTH_SHORT).show();
+                showMessage("Ingrese credenciales de acceso");
             }
         });
     }
 
-    private void openMenuPrincipal(String gamertag) {
+    private void openMenuPrincipal(Jugador jugador) {
         Intent intent = new Intent(this, MenuPrincipal.class);
-        intent.putExtra(MenuPrincipal.GAMERTAG_KEY, gamertag);
+        intent.putExtra(MenuPrincipal.GAMERTAG_KEY, jugador.getGamertag());
         startActivity(intent);
     }
 
-    private synchronized void loginUser(String gamertag, String password) {
+    private void loginUser(String gamertag, String password) {
         JugadorDAO.iniciarSesion(gamertag, password, new Callback<Jugador>() {
             @Override
             public void onResponse(Call<Jugador> call, Response<Jugador> response) {
                 // Manejar la respuesta exitosa aquí
                 Jugador jugador = response.body();
+                Log.d("MainActivity","Jugador: "+jugador);
                 if(jugador != null){
-                    jugadorObtenido = jugador;
-                    banderaUsuario = true;
-                    Log.d("TodoBien", "Jugador Recuperado: "+jugadorObtenido.getGamertag()+" "+jugadorObtenido.getPassword()+" "+jugadorObtenido.getPartidasGanadas()+" "+jugadorObtenido.getPartidasPerdidas()+" "+jugadorObtenido.getMazo()+" "+jugadorObtenido.getIdFoto());
+                    openMenuPrincipal(jugador);
                 }else{
-                    jugadorObtenido = null;
+                    showMessage("Credenciales de acceso incorrectas");
                 }
 
             }
 
-
             @Override
             public void onFailure(Call<Jugador> call, Throwable t) {
-                // Manejar el fallo aquí
-                banderaConexion = true;
+                showMessage("Error en la conexión");
             }
         });
+    }
+
+    private void showMessage(String msj){
+        Toast.makeText(this,msj, Toast.LENGTH_SHORT).show();
     }
 }
