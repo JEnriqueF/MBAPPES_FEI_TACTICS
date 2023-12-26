504aec5b5079446724be1654ff2214f172f94b71
Javier Durán Torres <118077906+TRENCH13@users.noreply.github.com>
2023-12-26 16:00:59 -0600

    GUI de partida
    Se realizó la GUI de la partida sin ningún error

---
+++ MbappesFEITactics/MbappesFEITactics/app/src/main/java/com/mbappesfeitactics/Vista/Partida.java
@@ -0,0 +1,16 @@
+package com.mbappesfeitactics.Vista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mbappesfeitactics.R;
+
+public class Partida extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_partida);
+    }
+}
