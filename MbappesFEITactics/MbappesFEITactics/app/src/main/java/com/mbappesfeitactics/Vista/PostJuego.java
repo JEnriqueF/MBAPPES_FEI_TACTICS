0ecb030aea88668cbb9c21aa6dff10d994476cab
Javier Durán Torres <118077906+TRENCH13@users.noreply.github.com>
2023-12-26 17:07:14 -0600

    GUI de post partida

---
+++ MbappesFEITactics/MbappesFEITactics/app/src/main/java/com/mbappesfeitactics/Vista/PostJuego.java
@@ -0,0 +1,16 @@
+package com.mbappesfeitactics.Vista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mbappesfeitactics.R;
+
+public class PostJuego extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_juego);
+    }
+}
