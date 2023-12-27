fefaa7e6a816c87236083ac384e3abb60fa8a599
Melchor22 <zs20015751@estudiantes.uv.mx>
2023-12-27 12:58:51 -0600

    Agregado apertura de Lobby

--- MbappesFEITactics/MbappesFEITactics/app/src/main/java/com/mbappesfeitactics/Vista/Lobby.java
+++ MbappesFEITactics/MbappesFEITactics/app/src/main/java/com/mbappesfeitactics/Vista/Lobby.java
@@ -2,15 +2,26 @@ package com.mbappesfeitactics.Vista;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.mbappesfeitactics.R;
+import com.mbappesfeitactics.databinding.ActivityLobbyBinding;
+import com.mbappesfeitactics.databinding.ActivityMainBinding;
 
 public class Lobby extends AppCompatActivity {
+
+    ActivityLobbyBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityLobbyBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_lobby);
+
+        binding.btnCancelar.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MenuP.class);
+            startActivity(intent);
+        });
     }
 }
--- MbappesFEITactics/MbappesFEITactics/app/src/main/java/com/mbappesfeitactics/Vista/ui/menu/MenuFragment.java
+++ MbappesFEITactics/MbappesFEITactics/app/src/main/java/com/mbappesfeitactics/Vista/ui/menu/MenuFragment.java
@@ -1,5 +1,6 @@
 package com.mbappesfeitactics.Vista.ui.menu;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,22 +11,30 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.mbappesfeitactics.Vista.CrearUsuario;
+import com.mbappesfeitactics.Vista.Lobby;
 import com.mbappesfeitactics.databinding.FragmentMenuBinding;
 
 public class MenuFragment extends Fragment {
 
     private FragmentMenuBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        MenuViewModel menuViewModel =
-                new ViewModelProvider(this).get(MenuViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MenuViewModel menuViewModel = new ViewModelProvider(this).get(MenuViewModel.class);
 
-        binding = FragmentMenuBinding.inflate(inflater, container, false);
+        binding = FragmentMenuBinding.inflate(getLayoutInflater());
         View root = binding.getRoot();
 
         final TextView textView = binding.textMenu;
         menuViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        binding.btnJugar.setOnClickListener(v -> {
+            Intent intent = new Intent(requireActivity(), Lobby.class);
+            requireActivity().startActivity(intent);
+        });
+
+
         return root;
     }
 
